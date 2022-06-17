c8a12d394f0fdc166fb5dcda702e0b3617c0bfe0
kimjinhee <kjh11071@gmail.com>
2022-06-17 19:39:53 +0900

    apis/ main 수정

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+
+const Main = ()=>{
+    <></>
+
+    return(
+        <>
+        div
+        </>
+    )
+}
+
+export default Main;
