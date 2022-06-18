5775364af0343caa878451ad0d944ee2041468e5
kimjinhee <kjh11071@gmail.com>
2022-06-18 09:28:49 +0900

    Main/ Card 수정중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom';
-
+import{Card} from '../components/Card'
 
 const Main = () => {
     <>
