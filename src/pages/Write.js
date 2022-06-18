25ed83591ff40dcdc139a53889953340871263fb
kimjinhee <kjh11071@gmail.com>
2022-06-18 14:06:25 +0900

    CK에디터 준비

---
+++ src/pages/Write.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, useRef, useSelector } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+const Write = (props) => {
+    <></>
+
+    return (
+        <>
+<div className="Write box">
+    
+</div>
+        </>
+    )
+}
+
+export default Write
