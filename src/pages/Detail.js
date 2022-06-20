90e7dd2879a53c7bdaaa7e916e8cdfee0a4de05a
hongsoom <hongsoom@kakao.com>
2022-06-20 18:55:37 +0900

    Main, Mypage, Modal, Header, Card css

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,19 +2,36 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-
+import Comment from "../components/Comment";
+import card from "../assets/card.png";
+import "../css/Detail.css"
 
 const Detail = (list) => {
-    <></>
-return (
-    <>
-    <div className="postDtail">
-        디테일 내용
-    </div>
     
+    return (
+        <>
+    <div className="post-container">
+        <div className="post-box">
+            <div className="post-detail">
+                <img src={card} alt="게시글 사진" />
+                <div className="post-infor">
+                    <p>제목</p>
+                    <p>가격</p>
+                </div>
+            </div>
+            <div className="post-content">
+                게시글의 내용
+            </div>
+            <div className="post-avg">
+                <p>별점 및 평균</p>
+                <p>몇개 후기</p>
+            </div>
+            <div className="post-comment">
+              
+            </div>
+        </div>
+    </div>
     </>
-)
-
-
+    )
 }
 export default Detail;
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Card from "../components/Card";
-import profile from "../assets/profile.png";
+import profile from "../assets/user.png";
 import "../css/Mypage.css";
 import { BiEditAlt } from "react-icons/bi";
+import Modal from "../components/Modal";
 
 const Mypage = () => {
+
+    const [modalOpen, setModalOpen] = React.useState(false);
+
+    const openModal = () => {
+      setModalOpen(true);
+    };
+    const closeModal = () => {
+      setModalOpen(false);
+    };
 
     return (
         <div className="Mypage-container">
@@ -12,10 +22,11 @@ const Mypage = () => {
                 <div className="user-content">
                     <div className="user">
                         <img src={profile} alt="프로필"/>
-                        <button><BiEditAlt size="25" /></button>
                     </div>
                     <div className="nickname-title">
                         <p>닉네임 자리입니다.</p>
+                        <button onClick={openModal}><BiEditAlt size="30" /></button>
+                        <Modal open={modalOpen} close={closeModal} header="수정하기"></Modal>
                     </div>
                 </div>
                 <div className="post-content">
